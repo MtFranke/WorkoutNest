1431becc2afcc570ce44bb688b984d68a3885235
MtFranke <mateusz.franke@proton.me>
2023-12-15 19:54:36 +0100

    smoke test

--- src/WorkoutNest.API/Program.cs
+++ src/WorkoutNest.API/Program.cs
@@ -1,21 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
-using WorkoutNest;
 using WorkoutNest.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services
+    .AddCors();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 
 app.MapGet("/", () => "Hello World!");
 app.MapPost("auth/register", (Registration newUser) => { return newUser; });
